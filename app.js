646f173d72efe253bec6823ba25295be9f189ed9
sp4-rta <aakarshbeohar2004@gmail.com>
2023-04-30 22:46:08 +0530

    Fixed database (still needs improvemen)

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const date = require(__dirname + "/date.js");
+const date = require("./views/date.js");
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/worklistDB', {useNewUrlParser: true, useUnifiedTopology: true});
 const workSchema = new mongoose.Schema({
-    task: {
-        type: String,
-        required: [true, "Task must be added"]
-    }
+    task: String
 })
+
+// const topicSchema = {
+//     name: String,
+//     items: [workSchema]
+// };
+
+// const Topic = mongoose.model("Topic", topicSchema);
+
+
 const Task = mongoose.model('myTask', workSchema);
 const task1 = new Task({task: 'Workout'});
 const task2 = new Task({task: 'Meditate'});
@@ -23,36 +29,54 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-    // const day = date.getDate();
+    const day = date.getDate();
     Task.find().exec().then( (tasks) => {
         if (tasks.length === 0) {
             Task.insertMany(databaseList)
             res.redirect("/");
         } else {
-            res.render('index', { listTitle : "Today", newListItem: databaseList});
+            res.render('index', { listTitle : day, newListItem:
+            databaseList});
         }
     })
-})
+});
+
+// app.get("/:customListName", (req, res) => {
+//     const topic = req.params.customListName;
+//     // const newTopic = new Topic({name: topic, items:     databaseList});
+//     // newTopic.save();
+//     Topic.find({name: topic}).exec().then( (ele) => {
+//         if (topic === ele.name) {
+//             res.render("index", {listTitle : topic, newListItem:
+//             databaseList})
+//         } else {
+//             const newTopic = new Topic({name: topic, items:     databaseList});
+//             newTopic.save();
+//             res.redirect("/" + topic);
+//         }
+//     });
+// });
+
+app.post('/delete', (req, res) => {
+    var checkedItemId = req.body.checkbox;
+    Task.deleteOne({task: checkedItemId}).then((e) => {
+        console.log(e);
+    });
+    databaseList.filter(element => {
+        element.task !== checkedItemId
+    })
+    res.redirect("/");
+});
 
 app.post('/', (req, res) => {
     const item = req.body.task;
-    if (req.body.list === 'Work List') {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        const newItem = new Task({task: item});
-        newItem.save()
-        databaseList.push(newItem);
-    }
+    const newItem = new Task({task: item});
+    newItem.save()
+    databaseList.push(newItem);
     res.redirect("/");
-})
-
-
-app.get('/work', (req, res) => {
-    res.render("index", {listTitle : "Work List", newListItem: workItems});
-});
+    }
+)
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
